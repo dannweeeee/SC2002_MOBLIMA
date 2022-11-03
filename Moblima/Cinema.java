99a070ae4bcf42443136eee2d63a2769366f78da
marcusverykho <marcuskho@hotmail.com>
2022-11-04 04:16:16 +0800

    Removed seats from cinema

--- Moblima/Cinema.java
+++ Moblima/Cinema.java
@@ -10,7 +10,6 @@ public class Cinema {
 	    private ArrayList<Show> shows;
 	    private Cineplex cinemplex;
 	    private cineplexHandler cineplexHandler;
-	    private ArrayList<Seats> seats;
 
 	    public Cinema(String classtype,int seat_capacity) {
 	        idCounter += 1;
@@ -18,7 +17,6 @@ public class Cinema {
 	        this.classtype= classtype;
 	        this.seat_capacity = seat_capacity;
 	        this.shows = new ArrayList<>();
-	        this.seats = seatList();
 	    }
 
 	    public ArrayList<Show> getShows(){
@@ -28,32 +26,7 @@ public class Cinema {
 	    public int getCapacity() {
 	        return seat_capacity;
 	    }
-	    public ArrayList<Seats> seatList(){
-			ArrayList<Seats> allseats = new ArrayList<Seats>();
-			int cols = 10;
-			int rows = this.seat_capacity / cols;
 
-			for (int i = 0; i < rows; i++){
-				for(int j = 0; j < cols; j++){
-					Seats temp = new Seats(String.valueOf((char)(i + 65)), Integer.toString(j));
-					allseats.add(temp);
-				}
-			}
-			return allseats;
-		}
-	    public void removeSeats(Seats seat){
-			System.out.println("removing seat: " + seat.getSeat());
-			for (Seats s : this.seats){
-				if (s.getSeat().equals(seat.getSeat())){
-					s.setRow("X");
-					s.setCol("X");
-					break;
-				}
-			}
-		}
-	    public ArrayList<Seats> getSeatList(){
-			return this.seats;
-		}
 	    public int getCinemaID() {
 	    	return id;
 	    }
