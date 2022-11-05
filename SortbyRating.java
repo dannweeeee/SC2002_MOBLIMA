cb2feb5ff2f17057435cd6d6b49cecbf191c609a
Dann Wee <dannweezj@gmail.com>
2022-11-06 03:11:17 +0800

    deleted extra files

--- SortbyRating.java
+++
@@ -1,9 +0,0 @@
-package Moblima;
-import java.util.Comparator;
-public class SortbyRating implements Comparator<Movie> {
-	@Override
-    public int compare(Movie o1, Movie o2) {
-
-        return Double.compare(o1.getAverageRatings(), o2.getAverageRatings());
-}
-}
