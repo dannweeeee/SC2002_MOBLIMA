caf079f864c666d5f0180b17fb01a77a2cdd1e10
marcusverykho <marcuskho@hotmail.com>
2022-11-09 20:06:19 +0800

    Add files via upload

--- Moblima/DataBase/ExampleAdder.java
+++ Moblima/DataBase/ExampleAdder.java
@@ -7,10 +7,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import Moblima.Entities.Cinema;
 import Moblima.Entities.Cineplex;
 import Moblima.Entities.Movie;
-import Moblima.Entities.Show;
-import Moblima.Entities.User;
 import Moblima.Entities.Cinema.HallType;
 import Moblima.Handlers.CinemaHandler;
 import Moblima.Handlers.CineplexHandler;
@@ -27,35 +26,44 @@ public class ExampleAdder {
 		MovieHandler movieHandler = MovieHandler.getInstance();
 		ShowHandler showHandler = ShowHandler.getInstance();
 		SeatHandler seatHandler = SeatHandler.getInstance();
-		
-		Cineplex jurong = cineplexHandler.addCineplex("JurongPoint");
-		CinemaHandler JurongPoint = new CinemaHandler("JurongPoint");
-		JurongPoint.addCinema(HallType.STANDARD, 35, jurong);
-		JurongPoint.addCinema(HallType.PREMIUM, 10, jurong);
-		cineplexHandler.getAllCineplex().get(cineplexHandler.getSize()-1).setHall(JurongPoint);
-		JurongPoint.addCinema(HallType.VIP, 1, jurong);
-		
-		User ayush = new User("Ayush","ayus@gmail.com",3293131);
-
+		CinemaHandler cinemaHandler = CinemaHandler.getInstance();
+	  
+		Cineplex jurong = cineplexHandler.addCineplex("JurongPoint", cinemaHandler);
+	  
+		cinemaHandler.addCinema(HallType.STANDARD, 35, jurong);
+		cinemaHandler.addCinema(HallType.PREMIUM, 10, jurong);
+		cinemaHandler.addCinema(HallType.VIP, 1, jurong);
+	  
 		//Movie ironMan = new Movie("Iron Man","showing","Jon Favreaue","AAA", "Example Cast...", movieHandler);
-       // Movie avengers = new Movie("Avengers: End Game", "showing","Jon Favreaue","BBB", "Example Cast...", movieHandler);
-        String dateInString = "23/12/2020 09:00:00 AM";
+		// Movie avengers = new Movie("Avengers: End Game", "showing","Jon Favreaue","BBB", "Example Cast...", movieHandler);
+
+		String dateInString = "23/12/2020 09:00:00 AM";
 		try {
-			Date date = formatter.parse(dateInString);
-			Show show1 = showHandler.addShows(date, movieHandler.getMovie().get(0), cineplexHandler.getAllCineplex().get(0).getHall().get(0), seatHandler);
-			Show show2 = showHandler.addShows(date,movieHandler.getMovie().get(1),cineplexHandler.getAllCineplex().get(0).getHall().get(0), seatHandler);
-			Show show3 = showHandler.addShows(date,movieHandler.getMovie().get(1),cineplexHandler.getAllCineplex().get(0).getHall().get(1), seatHandler);
-			Show show4 = showHandler.addShows(date,movieHandler.getMovie().get(1),cineplexHandler.getAllCineplex().get(0).getHall().get(2), seatHandler);
+		 	Date date = formatter.parse(dateInString);
+			showHandler.addShows(date, movieHandler.getMovie().get(0), CinemaHandler.getInstance().getCinemaFromCineplex(jurong).get(0), seatHandler);
+			showHandler.addShows(date,movieHandler.getMovie().get(1), CinemaHandler.getInstance().getCinemaFromCineplex(jurong).get(0), seatHandler);
+			showHandler.addShows(date,movieHandler.getMovie().get(1), CinemaHandler.getInstance().getCinemaFromCineplex(jurong).get(0), seatHandler);
+			showHandler.addShows(date,movieHandler.getMovie().get(1), CinemaHandler.getInstance().getCinemaFromCineplex(jurong).get(0), seatHandler);
 		} catch (ParseException e) {
-			e.printStackTrace();
+		 e.printStackTrace();
 		}
-	}
+	   }
 	
 	public static void showExample() {
 		CineplexHandler cineplexHandler = CineplexHandler.getInstance();
+		ShowHandler showHandler = ShowHandler.getInstance();
+		
+		System.out.println("Show all cineplexes:");
 		cineplexHandler.printAllCineplex();
-		System.out.println("\nShow all cinemas:");
-		cineplexHandler.getAllCineplex().get(0).printAllCinema();
+		System.out.println("\n\nShow all cinemas:");
+		for (Cineplex temp : cineplexHandler.getAllCineplex()) {
+			for (Cinema c : CinemaHandler.getInstance().getCinemaFromCineplex(temp)){
+			 System.out.println(c);
+			}
+		   }
+		System.out.println("\nShow all shows:");
+		System.out.println(showHandler.getAllShows());
+		
 	}
 	
 	public static void readMovieFromTextFile(String fileName) throws FileNotFoundException{
