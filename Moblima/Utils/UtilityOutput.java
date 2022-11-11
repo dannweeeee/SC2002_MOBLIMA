1abc11e49bb02fa89c26f0bd34d30a91d7b6532e
marcusverykho <marcuskho@hotmail.com>
2022-11-11 20:32:29 +0800

    added Output

--- Moblima/Utils/UtilityOutput.java
+++ Moblima/Utils/UtilityOutput.java
@@ -1,13 +1,9 @@
 package Moblima.Utils;
 
+import java.util.ArrayList;
+
 public class UtilityOutput {
     public UtilityOutput(){}
-
-    public static void printMenu(String[] menu){
-        for (String s : menu){
-            System.out.println(s);
-        }
-    }
 
     public static void printMessage(String message){
         System.out.println(message);
@@ -15,5 +11,17 @@ public class UtilityOutput {
 
     public static void printInputMessage(String question){
         System.out.print(question);
+    }
+
+    public static void printMenu(String[] menu){
+        for (String s : menu){
+            UtilityOutput.printMessage(s);
+        }
+    }
+
+    public static void printObjectList(ArrayList<?> objList){
+        for (Object o : objList){
+            UtilityOutput.printMessage(o.toString());
+        }
     }
 }
