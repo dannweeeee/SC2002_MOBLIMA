4d56ab753271a4d7f608c0bef80ca611d61b95c8
marcusverykho <marcuskho@hotmail.com>
2022-11-12 02:28:10 +0800

    JAVA DOCS

--- Docs/member-search-index.js
+++ Docs/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"Moblima","c":"MovieBookerApp","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"Moblima","c":"MovieBookerApp","l":"MovieBookerApp()","u":"%3Cinit%3E()"},{"p":"Moblima","c":"MovieBookerApp","l":"showUserView(MovieBookerInterface)","u":"showUserView(Moblima.MovieBookerInterface)"}];updateSearchResults();
+memberSearchIndex = [{"p":"Moblima.Exceptions","c":"ShowNotFoundException","l":"ShowNotFoundException(String)","u":"%3Cinit%3E(java.lang.String)"}];updateSearchResults();
--- Moblima/Exceptions/ShowNotFoundException.java
+++ Moblima/Exceptions/ShowNotFoundException.java
@@ -1,6 +1,15 @@
 package Moblima.Exceptions;
 
+/**
+ * Exception class for Shows not found
+ * @author Marcus
+ * @version 1.0
+ */
 public class ShowNotFoundException extends Exception {
+    /**
+     * Default constructor for ShowNotFoundException
+     * @param errorMsg errorMessage to be printed
+     */
     public ShowNotFoundException(String errorMsg){
         super(errorMsg);
     }
